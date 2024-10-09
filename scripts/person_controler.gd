d1778d678354cd66d42df445935667f6c73a7917
Atlls <alejandroaigner1999@hotmail.com>
2024-10-08 23:43:02 -0400

    FIX animación bugeada en bucle
    Era el pinche AnimationTree que en las transiciones de la máquina de
    estados había que ponerla en "enable".

--- scripts/person_controler.gd
+++ scripts/person_controler.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-#export(int) var speed = 80.0
+var speed = 40.0
 
 # Llamado cuando el nodo entra en el árbol de la escena por primera vez.
 func _ready() -> void:
@@ -10,9 +10,11 @@ func e_run(value: Vector2) -> void:
 	$AnimationTree.get("parameters/playback").travel("run")
 	$AnimationTree.set("parameters/run/blend_position", value)
 	print(value)
+	print('Run')
 
 func e_idle() -> void:
 	$AnimationTree.get("parameters/playback").travel("idle")
+	print('Idle')
 
 
 # Llamado cada frame. 'delta' es el tiempo transcurrido desde el frame anterior.
@@ -21,7 +23,7 @@ func _physics_process(delta: float) -> void:
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	velocity = input_vector * 80.0
+	velocity = input_vector * speed
 	
 	if input_vector != Vector2.ZERO:
 		e_run(input_vector)
