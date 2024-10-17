f8cc01421b9534b5bbc83e6a665e954181eee494
Syndrast <umendoza2078@gmail.com>
2024-10-17 06:52:12 -0400

    feat: added cooldown to drill and editor warnings

--- drill.gd
+++ drill.gd
@@ -1,9 +1,18 @@
 extends RayCast2D
 
-# Llamado cada frame. 'delta' es el tiempo transcurrido desde el frame anterior.
+var _canDrill : bool = true
+@onready var _cooldown: Timer = $CoolDown
+
+
 func _physics_process(_delta: float) -> void:
 	
-	if is_colliding():
+	if is_colliding() and _canDrill:
 		var map: DestructibleTileMapLayer = get_collider()
 		var collision_position := get_collision_point() 
-		map.damage_tile(collision_position)
+		map.damage_tile(collision_position, 1)
+		_canDrill = false
+		_cooldown.start()
+
+
+func _on_cool_down_timeout() -> void:
+	_canDrill = true
