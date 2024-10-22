045b6663fc35afa387fc9e0b629c03e898eb0a6d
ateluz <alejandroaigner@hotmail.com>
2024-10-21 23:33:54 -0400

    feat: drill rotation using signals

---
+++ scripts/drill.gd
@@ -0,0 +1,36 @@
+extends RayCast2D
+
+var _canDrill : bool = true
+@onready var _cooldown: Timer = $CoolDown
+@onready var Vartolo: CharacterBody2D = $".."
+
+func _ready():
+	Vartolo.character_movement.connect(_on_vartolo_movement)
+
+func _physics_process(_delta: float) -> void:
+
+	if is_colliding() and _canDrill:
+		var map: DestructibleTileMapLayer = get_collider()
+		var collision_position := get_collision_point() 
+		map.damage_tile(collision_position, 1)
+		_canDrill = false
+		_cooldown.start()
+		
+func _on_cool_down_timeout() -> void:
+	_canDrill = true
+
+func _on_vartolo_movement() -> void:
+	var input_vector = Vartolo.direction
+	match input_vector:
+		Vector2(1, 0):
+			print('Right')
+			rotation_degrees = 270
+		Vector2(-1, 0):
+			print('Left')
+			rotation_degrees = 90
+		Vector2(0, 1):
+			print('Down')
+			rotation_degrees = 0
+		Vector2(0, -1):
+			print('Up')
+			rotation_degrees = 180
--- scripts/person_controler.gd
+++ scripts/person_controler.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
+signal character_movement
 @export var speed: float = 100.0
-
 @onready var _animation_tree: AnimationTree = $AnimationTree
-
-
+var direction: Vector2 = Vector2.ZERO
 # Llamado cuando el nodo entra en el árbol de la escena por primera vez.
 func _ready() -> void:
 	$AnimationPlayer.play('idle')
-
 
 # Llamado cada frame. 'delta' es el tiempo transcurrido desde el frame anterior.
 func _physics_process(_delta: float) -> void:
@@ -16,20 +14,24 @@ func _physics_process(_delta: float) -> void:
 	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	input_vector = input_vector.normalized()
+	#print(input_vector)
 	velocity = input_vector * speed
 	
 	if input_vector != Vector2.ZERO:
 		e_run(input_vector)
+		character_movement.emit()
+		direction = input_vector
 	else:
 		e_idle()
+		direction = Vector2.ZERO
 	
 	move_and_slide()
 
-
+# TODO: Hacer que el personaje siempre mire para un lado
+# cuando se detiene
 func e_run(value: Vector2) -> void:
 	_animation_tree.get("parameters/playback").travel("run")
 	_animation_tree.set("parameters/run/blend_position", value)
-
 
 func e_idle() -> void:
 	_animation_tree.get("parameters/playback").travel("idle")
