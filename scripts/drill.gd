726a96f185d882436c7cfdce717c5edc0862bf3c
ateluz <alejandroaigner@hotmail.com>
2024-10-24 00:55:30 -0400

    fix: bad collition

--- scripts/drill.gd
+++ scripts/drill.gd
@@ -10,9 +10,10 @@ func _ready():
 func _physics_process(_delta: float) -> void:
 
 	if is_colliding() and _canDrill:
-		var map: DestructibleTileMapLayer = get_collider()
-		var collision_position := get_collision_point() 
-		map.damage_tile(collision_position, 1)
+		var map = get_collider()
+		if map is DestructibleTileMapLayer:
+			var collision_position := get_collision_point() 
+			map.damage_tile(collision_position, 1)
 		_canDrill = false
 		_cooldown.start()
 		
